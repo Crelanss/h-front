5716d294bde27710429198dd60d764ee010488c2
b.buldaev <Buldaev2000@mail.ru>
2020-09-26 02:54:51 +0700

    test

--- src/index.js
+++ src/index.js
@@ -11,6 +11,11 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+const test = () => {
+    fetch("https://hknt-backend-prod.herokuapp.com/api/test").then(json => console.log(json))
+}
+test()
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
